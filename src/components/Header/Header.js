e646fe4e27415e41a4b17c0fb93785077d113ade
NiteChill <puissantachille@gmail.com>
2023-03-03 02:26:37 +0100

    header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,26 @@
+import { NavLink } from 'react-router-dom';
+
 import header from '../../assets/img/header.png'
 import styles from './Header.module.scss';
 
 export default function Header() {
   return (
-    <div alt='header' src={header} className={`d-flex-row w-full h-350 jc-space-between ai-flex-end p-24 bora-36 ${styles.header}`}>
+    <div
+      alt='header'
+      src={header}
+      className={`d-flex-row w-full h-350 jc-space-between ai-flex-end p-24 bora-36 ${styles.header}`}
+    >
       <div>
-        <p>Donit</p>
-        <p>Your wonderful to-do list</p>
+        <p className='c-on-surface fs-large fw-500 ff-primary-font'>Donit</p>
+        <p className='c-on-surface fs-medium fw-500 ff-primary-font'>
+          Your wonderful to-do list
+        </p>
       </div>
-      <div>
-        <p>Get started</p>
-      </div>
+      <NavLink to='/my-todo' className={`d-flex-row ai-center jc-center pt-24 pr-36 pb-24 pl-36 bora-100 b-primary ${styles.hover}`}>
+        <p className='c-on-primary fs-normal-plus fw-500 ff-primary-font'>
+          Get started
+        </p>
+      </NavLink>
     </div>
-  )
+  );
 }
